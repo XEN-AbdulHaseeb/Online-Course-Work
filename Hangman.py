97419377c64e28391d0cf206d500aaa3b69ebbe1
AbdulHaseeb <abdulhaseebmohammed@gmail.com>
2022-12-04 12:40:03 +0530

    Allows user to keep guessing till they have won

--- Hangman.py
+++ Hangman.py
@@ -1,11 +1,16 @@
 import random
 word_list = ['baboon','biriyani','aldritch']
 chosen_word = random.choice(word_list)
-#print(chosen_word)
-guess = input('Guess a letter: ').lower()
+print(chosen_word)
+blank_list = []
 
-for i in range(0,len(chosen_word)):
-    if guess == chosen_word[i]:
-        print('Right')
-    else:
-        print('Wrong')
+for i in chosen_word:
+    blank_list += '_'
+print(blank_list)
+
+while '_' in blank_list:
+    guess = input('Guess a letter: ').lower()
+    for i in range(0,len(chosen_word)):
+        if guess == chosen_word[i]:
+            blank_list[i] = guess
+    print(blank_list)
