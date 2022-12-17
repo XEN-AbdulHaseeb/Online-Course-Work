b64dcbc9635d33a46443f4fbd1b080deceaaf670
AbdulHaseeb <abdulhaseebmohammed@gmail.com>
2022-12-17 19:18:25 +0530

    Added Colors

--- Intermediate/Turtle-Shapes.py
+++ Intermediate/Turtle-Shapes.py
@@ -1,6 +1,8 @@
+import random
 from turtle import Turtle, Screen
+import random
 
-
+colors = ['blue', 'green', 'dark orange', 'firebrick']
 def shape(the_turtle, sides, angle):
     for _ in range(sides):
         the_turtle.forward(100)
@@ -9,6 +11,7 @@ def shape(the_turtle, sides, angle):
 
 not_a_turtle = Turtle()
 for i in range(3, 11):
+    not_a_turtle.pencolor(colors[i % 4])
     shape(not_a_turtle, i, 360 / i)
 screen = Screen()
 screen.exitonclick()
