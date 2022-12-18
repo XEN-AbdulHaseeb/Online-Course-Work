423469920817ee094fdaa718368ef07fd922446e
AbdulHaseeb <abdulhaseebmohammed@gmail.com>
2022-12-18 17:51:14 +0530

    Added Random Colors

--- Intermediate/Turtle-Random Walk.py
+++ Intermediate/Turtle-Random Walk.py
@@ -1,13 +1,18 @@
- 
 from turtle import Turtle, Screen
 import random
-colors = ['blue', 'green', 'dark orange', 'cyan', 'pink']
+screen = Screen()
+screen.colormode(255)
 directions = [0, 90, 120, 270]
+
+
+def random_color():
+    color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+    return color
 
 
 def choose_path(the_turtle: Turtle):
     direction = random.choice(directions)
-    the_turtle.pencolor(random.choice(colors))
+    the_turtle.color(random_color())
     the_turtle.setheading(direction)
     the_turtle.forward(30)
 
@@ -18,5 +23,4 @@ tortoise.speed('slow')
 for _ in range(1000):
     choose_path(tortoise)
 
-screen = Screen()
 screen.exitonclick()
